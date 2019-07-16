371b8e30a692dc846086c3661d4a18a314af6f2c
JHTA <JHTA@JHTA-PC>
2019-07-16 16:24:51 +0900

    스프링 파일업로드 방식

--- src/main/java/com/newlecture/NoticeController.java
+++ src/main/java/com/newlecture/NoticeController.java
@@ -1,9 +1,15 @@
 package com.newlecture;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.sql.SQLException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,8 +19,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import com.newlecture.web.dao.NoticeDao;
+import com.newlecture.web.dao.oracle.Notice;
 
 @Controller("adminNoticeController")
 @RequestMapping("/admin/notice/")
@@ -79,9 +88,64 @@ public class NoticeController {
 
 	// post요청
 	@PostMapping("reg")
-	public String adsdsdf(String title) throws ClassNotFoundException, SQLException {
-		// 리디렉션 : list 페이지로
-		System.out.println("post");
+	//public String adsdsdf(Notice notice) throws ClassNotFoundException, SQLException {
+	//public String adsdsdf(String category,String title,String file, String content) throws ClassNotFoundException, SQLException {
+	public String adsdsdf(Notice notice,
+			String category,
+			MultipartFile file,
+			HttpServletRequest request
+			) throws ClassNotFoundException, SQLException, IOException {
+
+		System.out.println(category);
+		System.out.println(notice.getTitle());
+		System.out.println(notice.getContent());
+		System.out.println(file);
+		
+		// 1. 업로드 경로를 얻기
+	      String urlPath = "/upload";
+	      String path = request
+	                     .getServletContext()
+	                     .getRealPath(urlPath);
+	      
+	      System.out.println(path);
+	      
+	      // 2. 업로드된 파일명 얻기
+	      String fileName =file.getOriginalFilename();//filePart.getSubmittedFileName();
+	            
+	      // 3. 경로 구분자 넣기 
+	      String filePath = path + File.separator + fileName; // d:\aa + "bb.jpg" -> d:\aabb.jpg
+	      
+	      System.out.println(filePath);
+	      
+	      // 4. 경로가 없다는 오류 문제
+	      File pathFile = new File(path);
+	      if(!pathFile.exists()) // 존재하지 않으면
+	         pathFile.mkdirs();// 생성해주세요.  
+	      
+	       // 5. 여러파일을 받으려면 배열로 받는다
+	       //요즘은 js를 이용하기때문에 비동기적부분으로 파일업로드를 따로 만드는것이 트렌드이다
+	       File sameFile = new File(filePath);
+	        System.out.println(sameFile);
+	        if(sameFile.exists()) {
+	           
+	           int n = fileName.lastIndexOf(".");           // fileName=hello.jpg      n =?,    name=    , suffix,    
+	           String name = fileName.substring(0, n);  // 
+	           String suffix = fileName.substring(n);         
+	           
+	               fileName = name +"("+ 1 +")"+ suffix;
+	       }
+	      
+	      InputStream fis = file.getInputStream();//filePart.getInputStream();
+	      OutputStream fos = new FileOutputStream(filePath);
+	      
+	      byte[] buf = new byte[1024];
+	      int size = 0;
+	      while((size = fis.read(buf)) != -1)
+	         fos.write(buf, 0, size);
+	      
+	      fis.close();
+	      fos.close();
+		
 		return "redirect:list";
 	}
 }
--- src/main/java/com/newlecture/RootController.java
+++ src/main/java/com/newlecture/RootController.java
@@ -1,15 +1,23 @@
 package com.newlecture;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.newlecture.web.dao.NoticeDao;
 
@@ -53,4 +61,52 @@ public class RootController {
 		return "환영합니다";
 	}
 	
+	@PostMapping("upload")
+	@ResponseBody
+	public String upload(MultipartFile file, HttpServletRequest request) throws IOException {
+		  String urlPath = "/upload";
+	      String path = request
+	                     .getServletContext()
+	                     .getRealPath(urlPath);
+	      
+	      System.out.println(path);
+	      
+	      // 2. 업로드된 파일명 얻기
+	      String fileName =file.getOriginalFilename();//filePart.getSubmittedFileName();
+	            
+	      // 3. 경로 구분자 넣기 
+	      String filePath = path + File.separator + fileName; // d:\aa + "bb.jpg" -> d:\aabb.jpg
+	      
+	      System.out.println(filePath);
+	      
+	      // 4. 경로가 없다는 오류 문제
+	      File pathFile = new File(path);
+	      if(!pathFile.exists()) // 존재하지 않으면
+	         pathFile.mkdirs();// 생성해주세요.  
+	      
+	       // 5. 여러파일을 받으려면 배열로 받는다
+	       //요즘은 js를 이용하기때문에 비동기적부분으로 파일업로드를 따로 만드는것이 트렌드이다
+	       File sameFile = new File(filePath);
+	        System.out.println(sameFile);
+	        if(sameFile.exists()) {
+	           
+	           int n = fileName.lastIndexOf(".");           // fileName=hello.jpg      n =?,    name=    , suffix,    
+	           String name = fileName.substring(0, n);  // 
+	           String suffix = fileName.substring(n);         
+	           
+	               fileName = name +"("+ 1 +")"+ suffix;
+	       }
+	      
+	      InputStream fis = file.getInputStream();//filePart.getInputStream();
+	      OutputStream fos = new FileOutputStream(filePath);
+	      
+	      byte[] buf = new byte[1024];
+	      int size = 0;
+	      while((size = fis.read(buf)) != -1)
+	         fos.write(buf, 0, size);
+	      
+	      fis.close();
+	      fos.close();
+		return "파일업로드  처리기";
+	}
 }
